b926d9f87bede44bc642cd6279ca38d587b417eb
Cüneyd Gültekin Kaya <cgultekink@gmail.com>
2024-08-04 14:36:07 +0300

    Update experience.js

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,19 +1,19 @@
 export const experiences = [
   {
     id: 1,
-    title: 'Software Engineer I',
+    title: 'Software Tester',
     company: "Teton Private Ltd.",
     duration: "(Jan 2022 - Present)"
   },
   {
     id: 2,
-    title: "FullStack Developer",
+    title: "Software Tester",
     company: "Fiverr (freelance)",
     duration: "(Jun 2021 - Jan 2022)"
   },
   {
     id: 3,
-    title: "Self Employed",
+    title: "Software Tester",
     company: "Code and build something in everyday.",
     duration: "(Jan 2018 - Present)"
   }
